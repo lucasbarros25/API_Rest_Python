59bdf8e580c4153d81a708afa36c70fc3cc98522
Lucas Outor Barros <lucasoutor@gmail.com>
2020-09-28 11:55:23 -0300

    Enter

--- pythonrest/pythonrest/settings.py
+++ pythonrest/pythonrest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empregadoapi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -71,13 +72,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pythonrest.wsgi.application'
 
 
-# Database
+# Database CONNECTION IS HERE
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restapi',
+        'USER': 'postgres' ,
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
