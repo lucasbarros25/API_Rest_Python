47fd1d70640d11b2e9e57588570f4a8ba4e6392b
Lucas Outor Barros <lucasoutor@gmail.com>
2020-09-28 12:55:02 -0300

    Ente

---
+++ pythonrest/empregadoapi/seriealizer.py
@@ -0,0 +1,9 @@
+# Esse arquivo é uma parte importante da Api, ele serve pra converter a informação para um JSON para
+# poder mandar a informação para o que quer que seja. 
+from rest_framework import serializers
+from .models import Empregado
+
+Class SerEmpregado(serializers.ModelSerializer):
+    class Meta:
+        model = Empregado
+        field = '__all__' # Se quiser só um ou outro campo é só usar: ('id', 'name')
